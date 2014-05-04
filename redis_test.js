bbd17fe0f4fbc0c37bea62770ef6b77a0a8e5c07
ocowchun <ocowchun@gmail.com>
2014-05-04 17:11:08 +0800

    New: add redis tag

--- redis_test.js
+++ redis_test.js
@@ -1,22 +1,45 @@
-    var redis = require("redis"),
-        client = redis.createClient(6379,"192.168.2.8");
+    // var redis = require("redis"),
+    //     client = redis.createClient(6379, "192.168.2.8");
 
-    // if you'd like to select database 3, instead of 0 (default), call
-    // client.select(3, function() { /* ... */ });
+    // // if you'd like to select database 3, instead of 0 (default), call
+    // // client.select(3, function() { /* ... */ });
 
-    client.on("error", function (err) {
-        console.log("Error " + err);
-    });
+    // client.on("error", function(err) {
+    //     console.log("Error " + err);
+    // });
 
-    client.set("string key", "string val", redis.print);
-    client.get("foo", redis.print);
+    // client.set("string key1", "string val1", redis.print);
+    // client.get("foo", function(err, result) {
+    //     // console.log(result);
+    //     if (result == null) {
+    //         console.log("result is null");
 
-    client.hset("hash key", "hashtest 1", "some value", redis.print);
-    client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-    client.hkeys("hash key", function (err, replies) {
-        console.log(replies.length + " replies:");
-        replies.forEach(function (reply, i) {
-            console.log("    " + i + ": " + reply);
-        });
-        client.quit();
-    });
+    //     } else {
+    //         console.log("result is not null");
+
+    //     }
+    // });
+
+    // client.hset("hash key", "hashtest 1", "some value", redis.print);
+    // client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+    // client.hkeys("hash key", function(err, replies) {
+    //     console.log(replies.length + " replies:");
+    //     replies.forEach(function(reply, i) {
+    //         console.log("    " + i + ": " + reply);
+    //     });
+    //     client.quit();
+    // });
+
+    var tagManager = require('./lib/redis_model/tag');
+    var tag1 = {
+        name: 'good3',
+        count: 3
+    }, tag2 = {
+            name: 'bad2',
+            count: 2
+        },
+        tags = [tag1, tag2];
+    tagManager.update(tags);
+
+
+    // console.log("1"*1+1);

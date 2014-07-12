2f9b02bfb85be02ba345fab4e10c696a2fbb3052
ocowchun <ocowchun@gmail.com>
2014-07-12 22:04:30 +0800

    fix: the god damm small q

--- app/train.js
+++ app/train.js
@@ -6,7 +6,7 @@ var tagTitleManager = require('../lib/redis_model/tag_title');
 var docManager = require('../lib/models/doc');
 var docStorage = require('../lib/elastic_search/doc');
 
-var Q = require('Q');
+var Q = require('q');
 
 function getTextFromHtml(body) {
 	var validHTMLTags = /^(?:a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|bgsound|big|blink|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|data|datalist|dd|del|details|dfn|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|isindex|kbd|keygen|label|legend|li|link|listing|main|map|mark|marquee|menu|menuitem|meta|meter|nav|nobr|noframes|noscript|object|ol|optgroup|option|output|p|param|plaintext|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|spacer|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr|xmp)$/i;
